4aa8c43bdf2685d79e51b677a0c158f346786a5e
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-14 10:18:00 -0600

    erase story comments

--- spec/features/avatar_api_spec.rb
+++ spec/features/avatar_api_spec.rb
@@ -16,11 +16,3 @@ RSpec.describe "As a visitor" do
     expect(page).to have_css("#affiliation", count: 20)
   end
 end
-
-# Then I should be on page "/search"
-# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
-# And I should see a list with the detailed information for the 20 members of the Fire Nation.
-# And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies and a list of enemies
-# - Any affiliation that the member has
