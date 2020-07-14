bcdeeed8ef3a19c85c170209c4f2fc43f78a45d9
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-14 10:10:33 -0600

    erase comments

--- spec/features/avatar_api_spec.rb
+++ spec/features/avatar_api_spec.rb
@@ -16,11 +16,7 @@ RSpec.describe "As a visitor" do
     expect(page).to have_css("#affiliation", count: 20)
   end
 end
-# As a user,
-# When I visit "/"
-# And I Select "Fire Nation" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
+
 # Then I should be on page "/search"
 # Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
 # And I should see a list with the detailed information for the 20 members of the Fire Nation.
